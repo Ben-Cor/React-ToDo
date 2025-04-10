e61aae564d1d9fc27927e57505d36a5e0b816980
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-10 10:46:05 +0100

    submitButton made

---
+++ src/components/submitButton.jsx
@@ -0,0 +1,9 @@
+function submitButton({string: title}) {
+    return (
+        <button type="submit" id="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            {title}
+        </button>
+    )
+}
+
+export default submitButton;

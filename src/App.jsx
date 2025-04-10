751557c30f1c6752f287ca0b2c574a496508ee6d
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-10 14:11:54 +0100

    footer created and added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
@@ -12,11 +13,14 @@ function App() {
         <h1 className="text-3xl text-black font-bold underline">
           Hello world!
         </h1>
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+
+        <Footer />
 
     </BrowserRouter>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer className="
+        flex
+        items-center
+        justify-between
+        gap-2
+        px-8
+        bg-gray-800
+        text-white
+        p-4
+        ">
+            <p>Copyright Ben Cornell</p>
+            <a target={"_blank"} href={"https://github.com/Ben-Cor"}>Github</a>
+        </footer>
+    )
+}
+
+export default Footer;

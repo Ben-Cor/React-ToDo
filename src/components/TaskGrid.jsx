75d5779ec56009cfe1372dec44fc051048f5bac0
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-15 16:58:49 +0100

    gitignore updated and table tailwind updated

--- src/components/TaskGrid.jsx
+++ src/components/TaskGrid.jsx
@@ -1,24 +1,26 @@
 function TaskGrid() {
     return (
-        <section className="Flex items-center justify-center w-full">
-            <table className="w-full border border-gray-300 flex items-center justify-between">
-                <thead>
-                    <tr>
-                        <th>Task</th>
-                        <th>Status</th>
-                        <th>Categories</th>
-                        <th>Due Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Task name</td>
-                        <td>In Progress</td>
-                        <td><button>Edit</button></td>
-                        <td>2025-10-01</td>
-                    </tr>
-                </tbody>
-            </table>
+        <section className="flex items-center justify-center w-full">
+            <div className="flex justify-center w-full py-5">
+                <table className="w-[90vw]">
+                    <thead>
+                        <tr className="text-center border-b-2">
+                            <th>Task</th>
+                            <th>Status</th>
+                            <th>Categories</th>
+                            <th>Due Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="text-center border-t-2">
+                            <td>Task name</td>
+                            <td>In Progress</td>
+                            <td>Work</td>
+                            <td>2025-10-01</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     )
 }
